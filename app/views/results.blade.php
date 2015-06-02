cd95114db5c309e358b409c3b9d4e7816d307698
Jamie Haskell <jmehaskell@gmail.com>
2015-06-01 18:33:55 -0700

    html skeleton for displaying most common results

--- app/views/results.blade.php
+++ app/views/results.blade.php
@@ -1,10 +1,15 @@
 <!DOCTYPE html>
-
 <html>
 <head>
 	<title>Results</title>
 </head>
 <body>
-	<h1>Results page here</h1>
+	<ol>
+		@foreach($questions as $question)
+			<h3><li>{{{$question->question}}}</li></h3>
+			<h5>Most Common Male Response: {{{ $male3->answer }}}</h5>
+			<h5>Most Common Female Response: {{{ $female3->answer }}}</h5>
+		@endforeach
+	</ol>
 </body>
 </html>
