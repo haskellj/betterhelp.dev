cb4e16147689fa521d43f7b4f179e8d99ae42d25
Jamie Haskell <jmehaskell@gmail.com>
2015-06-01 18:11:55 -0700

    no changes

--- app/controllers/SurveyController.php
+++ app/controllers/SurveyController.php
@@ -9,15 +9,19 @@ class SurveyController extends BaseController {
 
 		foreach($questions as $question){
 			$type = $question->type;
-			$typeResults = Input::get($question->type);
-					
-			foreach($typeResults as $answer){
-				$survey = new Survey;
-				$survey->question_id = $question->id;
-				$survey->answer = $answer;
-				$survey->save();
+			$gender = Input::get('sex');
+
+			if($type != 'sex'){
+				$typeResults = Input::get($type);
+
+				foreach($typeResults as $answer){
+					$survey = new Survey;
+					$survey->gender = $gender[0];
+					$survey->question_id = $question->id; 
+					$survey->answer = $answer;
+					$survey->save();
+				}
 			}
-			
 		}
 		
 		return Redirect::to('stats');
