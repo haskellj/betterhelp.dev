10784f01200c728e7b650f11b6aec2045fe1c747
Jamie Haskell <jmehaskell@gmail.com>
2015-06-04 15:31:29 -0500

    added default ranks to each question

--- app/database/seeds/QuestionsTableSeeder.php
+++ app/database/seeds/QuestionsTableSeeder.php
@@ -8,26 +8,31 @@ class QuestionsTableSeeder extends Seeder {
 		$questions = [
 			[ 	
 				'type'				   => 'sex',
+				'rank'				   => 1,
 				'question' 			   => 'What is your gender?', 
 				'multiple_answers' 	   => false 
 			],
 			[ 	
 				'type'				   => 'relationship_status',
+				'rank'				   => 2,
 				'question' 			   => 'What is your relationship status?', 
 				'multiple_answers' 	   => false 
 			],
 			[ 	
 				'type'				   => 'countries',
+				'rank'				   => 3,
 				'question' 			   => 'Which countries have you visited?', 
 				'multiple_answers' 	   => true 
 			],
 			[ 	
 				'type'				   => 'sport',
+				'rank'				   => 4,
 				'question' 			   => 'What is your favorite sport?', 
 				'multiple_answers' 	   => false 
 			],
 			[ 	
 				'type'				   => 'languages',
+				'rank'				   => 5,
 				'question' 			   => 'Which programming languages do you know?', 
 				'multiple_answers' 	   => true 
 			]
