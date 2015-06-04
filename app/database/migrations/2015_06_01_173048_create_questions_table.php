931aafd8b9c124cb8b083d25ce001a0460e040f3
Jamie Haskell <jmehaskell@gmail.com>
2015-06-04 15:31:14 -0500

    added a rank column

--- app/database/migrations/2015_06_01_173048_create_questions_table.php
+++ app/database/migrations/2015_06_01_173048_create_questions_table.php
@@ -16,6 +16,7 @@ class CreateQuestionsTable extends Migration {
 		{
 			$table->increments('id');
 			$table->string('type');
+			$table->integer('rank')->unsigned();
 			$table->string('question');
 			$table->boolean('multiple_answers');
 			$table->timestamps();
