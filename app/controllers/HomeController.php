66e30ecb3ef0ac00815f7c0cbce34eda3f4baae1
Jamie Haskell <jmehaskell@gmail.com>
2015-06-04 15:31:01 -0500

    moved results logic to the Question model

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -2,22 +2,10 @@
 
 class HomeController extends BaseController {
 
-	/*
-	|--------------------------------------------------------------------------
-	| Default Home Controller
-	|--------------------------------------------------------------------------
-	|
-	| You may wish to use controllers instead of, or in addition to, Closure
-	| based routes. That's great! Here is an example controller method to
-	| get you started. To route to this controller, just add the route:
-	|
-	|	Route::get('/', 'HomeController@showWelcome');
-	|
-	*/
 
 	public function showSurvey()
 	{
-		$questions = Question::all();
+		$questions = Question::orderBy('rank', 'ASC')->get();
 		$answers = Answer::with('question')->get();
 
 		$data = [
@@ -30,37 +18,7 @@ class HomeController extends BaseController {
 
 	public function showResults()
 	{
-		$questions = Question::all();
-		$data = [];
-
-		foreach($questions as $question){
-			
-			if($question->type != 'sex'){
-				$mostCommonFemaleAnswer = Survey::select(array('answer', DB::raw('COUNT(*) `occurrence`')))
-										->where('question_id','=', $question->id)
-										->whereGender('Female')
-										->groupBy('answer')
-										->orderBy('occurrence', 'DESC')
-										->first();
-				
-				$mostCommonMaleAnswer = Survey::select(array('answer', DB::raw('COUNT(*) `occurrence`')))
-										->where('question_id','=', $question->id)
-										->whereGender('Male')
-										->groupBy('answer')
-										->orderBy('occurrence', 'DESC')
-										->first();
-			
-
-				$data["female$question->id"] = $mostCommonFemaleAnswer;
-				$data["male$question->id"] = $mostCommonMaleAnswer;
-			}
-			
-		}
-
-		$data['questions'] = $questions;
-
-		// var_dump($data);
-		// dd();
+		$data = Question::surveyResults();
 
 		return View::make('results')->with($data);
 	}
