218f691e6d4f742cce7d2ac563977cc1fda1e692
Jamie Haskell <jmehaskell@gmail.com>
2015-06-04 15:32:03 -0500

    added a surveyResults() method to process the logic for getting the most common male and female responses for each question

--- app/models/Question.php
+++ app/models/Question.php
@@ -25,6 +25,45 @@ class Question extends BaseModel {
 		// connects each survey to its questions
 		return $this->hasMany('Survey');
 	}
+
+	public static function surveyResults()
+	{
+		$questions = Question::all();
+
+		$data = [
+			'male'   => [],
+			'female' => []
+		];
+
+		foreach($questions as $question){
+			
+			if($question->type != 'sex'){
+				
+				$mostCommonFemaleAnswer = Survey::select(array('answer', DB::raw('COUNT(*) `occurrence`')))
+												->where('question_id','=', $question->id)
+												->whereGender('Female')
+												->groupBy('answer')
+												->orderBy('occurrence', 'DESC')
+												->first();
+				
+				$mostCommonMaleAnswer =   Survey::select(array('answer', DB::raw('COUNT(*) `occurrence`')))
+												->where('question_id','=', $question->id)
+												->whereGender('Male')
+												->groupBy('answer')
+												->orderBy('occurrence', 'DESC')
+												->first();
+			
+
+				$data["female"][$question->id] = $mostCommonFemaleAnswer;
+				$data["male"][$question->id] = $mostCommonMaleAnswer;
+			}
+			
+		}
+
+		$data['questions'] = $questions;
+
+		return $data;
+	}
 }
 
 
