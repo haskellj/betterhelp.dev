2c721b44142873a2b42f2deb1264e34972ab81d4
Jamie Haskell <jmehaskell@gmail.com>
2015-06-04 15:32:53 -0500

    simplified logic for displaying each questions most common response, also added a backup h5 tag for male and femal in case there are no responses for that gender yet

--- app/views/results.blade.php
+++ app/views/results.blade.php
@@ -4,12 +4,22 @@
 	<title>Results</title>
 </head>
 <body>
-	<ol>
+	<ul>
 		@foreach($questions as $question)
-			<h3><li>{{{$question->question}}}</li></h3>
-			<h5>Most Common Male Response: {{{ $male3->answer }}}</h5>
-			<h5>Most Common Female Response: {{{ $female3->answer }}}</h5>
+			@if($question->type != 'sex')
+				<h3><li>{{{$question->question}}}</li></h3>
+				@if(!empty($male[$question->id]))
+					<h5>Most Common Male Response: {{{ $male[$question->id]->answer }}}</h5>
+				@else
+					<h5>No male responses yet.</h5>
+				@endif
+				@if(!empty($female[$question->id]))
+					<h5>Most Common Female Response: {{{ $female[$question->id]->answer }}}</h5>
+				@else
+					<h5>No female responses yet.</h5>
+				@endif
+			@endif
 		@endforeach
-	</ol>
+	</ul>
 </body>
 </html>
